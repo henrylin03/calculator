4ee66476a930ef69f71d7380c59e085c9c5e1916
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-02-26 07:51:19 +1100

    add operate function, which takes in a parameter 'operator' to determine which of the four add, subtract, multiply, divide functions to call on operate's other two parameters, being the numbers

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            alert("Please input a valid operator - add (+), subtract (-), multiply (*) or divide (/)")
+    }
+}
+
 let num1;
 let num2;
 let operator;
+
+// console.log(operate(1, 1, "w"))
