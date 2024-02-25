b408198c143f56510a1e98e306f775c2c0f30a49
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-02-26 10:46:24 +1100

    rename variables for clarity
    'btn' is a fairly common abbrevation for button from what I've seen in codebases and tutorials. adding it as a suffix to the buttons distinguishes those dom elements to the display

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const digitButtons = document.querySelectorAll(".digit");
+const digitBtns = document.querySelectorAll(".digit");
 const display = document.querySelector(".display");
-const backspace = document.querySelector("#backspace");
+const backspaceBtn = document.querySelector("#backspace");
+const clearBtn = document.querySelector("#clear");
 
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
@@ -36,8 +37,9 @@ let operator;
 
 // ADD EVENT LISTENER ON DIGIT BUTTONS SUCH THAT WHEN THEY ARE CLICKED, THEY ARE SHOWN ON DISPLAY. THEN CAN BE REMOVED. IM NOT SURE IF THIS IS THE RIGHT THING VS ADDING EVENT LISTENER ON DISPLAY... NO I DON'T THINK SO. IT SHOULD BE THE BUTTONS.
 
-digitButtons.forEach(button => {
-    button.addEventListener("click",
-        () => display.textContent += button.textContent
+digitBtns.forEach(btn => {
+    btn.addEventListener("click",
+        () => display.textContent += btn.textContent
     )
 })
+// backspace.addEventListener("click", () => display.textContent = display.textContent)
