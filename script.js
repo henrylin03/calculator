270ac1ff2d9127abc1a73b53afb9836e4ebe22f3
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-02-26 07:46:05 +1100

    add variables for the first number, operator, then second number

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+
+let num1;
+let num2;
+let operator;
