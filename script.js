ee1eabe0b96b63886d3a3dab41416f161325b9dd
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-02-26 14:11:17 +1100

    add event listener on each of the operator buttons (plus, minus, multiple, divide), and on click on any of them, capture the first number and operator

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const display = document.querySelector(".display");
 const digitBtns = document.querySelectorAll(".digit");
+const operatorBtns = document.querySelectorAll(".operator");
 const backspaceBtn = document.querySelector("#backspace");
 const clearBtn = document.querySelector("#clear");
 
@@ -14,12 +15,10 @@ function operate(num1, num2, operator) {
             return add(num1, num2);
         case "-":
             return subtract(num1, num2);
-        case "*":
+        case "×":
             return multiply(num1, num2);
-        case "/":
+        case "÷":
             return divide(num1, num2);
-        default:
-            alert("Please input a valid operator - add (+), subtract (-), multiply (*) or divide (/)")
     }
 }
 
@@ -39,3 +38,25 @@ clearBtn.addEventListener("click", () => display.textContent = "");
 let num1;
 let num2;
 let operator;
+
+operatorBtns.forEach(btn => {
+    btn.addEventListener("click", handleClick)
+
+    function handleClick(e) {
+        if (!display) return;
+
+        num1 = display.textContent;
+        operator = e.target.textContent;
+        const res = [num1, operator]
+        console.log(res);
+        return res;
+    }
+})
+
+// have operators listen for clicks
+// WHEN user clicks any operator (that isn't equals):
+// IF there is no value in display, do nothing
+// IF there is value in display, store the value as num1, and operator as `operator`
+// display does not change.
+// WHEN user clicks equals WITHOUT having touched any of the other numbers, nothing happens
+// IF user has clicked any of the other digits (incl 0), then return answer
